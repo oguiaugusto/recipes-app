ad4a23076278365287b4e1f55181113ad2058e63
Guilherme Santos <guiafsantos.dev@gmail.com>
2025-09-14 17:18:50 -0300

    localStorage saveUser and getUser helper functions

---
+++ src/helpers/localStorage.ts
@@ -0,0 +1,10 @@
+import type { IUser } from '../interfaces/user';
+
+export function saveUser(user: IUser) {
+  localStorage.setItem('user', JSON.stringify(user));
+}
+
+export function getUser(): IUser | null {
+  const user = localStorage.getItem('user');
+  return user ? (JSON.parse(user) as IUser) : null;
+}
