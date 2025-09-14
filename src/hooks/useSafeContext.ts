8d3dd1c65c436c459f5efa3f2300d92a0315d448
Guilherme Santos <guiafsantos.dev@gmail.com>
2025-09-14 17:06:18 -0300

    useSafeContext hook

---
+++ src/hooks/useSafeContext.ts
@@ -0,0 +1,14 @@
+import type { Context } from 'react';
+import { useContext } from 'react';
+
+function useSafeContext<T>(context: Context<T | null>): T {
+  const ctx = useContext(context);
+
+  if (!ctx) {
+    throw new Error('useSafeContext must be used within a Provider with a valid value');
+  }
+
+  return ctx;
+}
+
+export { useSafeContext };
