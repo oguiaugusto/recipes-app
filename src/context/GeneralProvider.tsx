643e9a01843f7f0bd7f410b0f216860f3dc23bad
Guilherme Santos <guiafsantos.dev@gmail.com>
2025-09-28 17:36:39 -0300

    saves favorite and done recipes arrays to local storage if they don't exist when logging in

--- src/context/GeneralProvider.tsx
+++ src/context/GeneralProvider.tsx
@@ -1,13 +1,23 @@
 import { PropsWithChildren, useState } from 'react';
 import { GeneralContext } from './GeneralContext';
 import { Loader } from '../components/Loader';
-import { saveToStorage } from '../helpers/localStorage';
+import { getFromStorage, saveToStorage } from '../helpers/localStorage';
 
 const GeneralProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [loading, setLoading] = useState(false);
 
   const handleLogin = (name: string) => {
     saveToStorage('user', { name });
+
+    const favoriteRecipes = getFromStorage('favoriteRecipes');
+    const doneRecipes = getFromStorage('doneRecipes');
+
+    if (!favoriteRecipes) {
+      saveToStorage('favoriteRecipes', []);
+    }
+    if (!doneRecipes) {
+      saveToStorage('doneRecipes', []);
+    }
   };
 
   const value = {
