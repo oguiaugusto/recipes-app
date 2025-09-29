4b62e072cd938c17562484b097c7fb332931792e
Guilherme Santos <guiafsantos.dev@gmail.com>
2025-09-28 23:22:03 -0300

    categories navigation bar working at header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { LuUserRound } from 'react-icons/lu';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { Button, Form, InputGroup } from 'react-bootstrap';
+import { Button, ButtonGroup, Form, InputGroup, ToggleButton } from 'react-bootstrap';
+import { ICategory } from '../../interfaces/misc';
 import '../../styles/Header.scss';
 
 enum SearchSelector {
@@ -13,14 +14,17 @@ enum SearchSelector {
 type Props = {
   title: string;
   hideSearch?: boolean;
+  categories?: ICategory[];
 };
 
 const Header: React.FC<Props> = (props) => {
-  const { title, hideSearch } = props;
+  const { title, hideSearch, categories } = props;
 
   const [expandedSearchBar, setExpandedSearchBar] = useState(false);
   const [searchValue, setSearchValue] = useState('');
   const [searchSelector, setSearchSelector] = useState<number>(-1);
+
+  const [category, setCategory] = useState('All');
 
   const searchSelectors = [
     { index: 0, label: SearchSelector[0] },
@@ -70,10 +74,32 @@ const Header: React.FC<Props> = (props) => {
     </div>
   );
 
+  const renderCategories = () => (
+    <div className="header-categories">
+      <ButtonGroup>
+        {categories!.map((x, i) => (
+          <ToggleButton
+            key={`category-${i}`}
+            type="radio"
+            size="sm"
+            variant={category === x.strCategory ? 'secondary' : 'dark'}
+            title={x.strCategory}
+            id={`${x.strCategory}-${i}`}
+            value={category}
+            checked={category === x.strCategory}
+            onChange={() => setCategory(x.strCategory)}
+          >
+            {x.strCategory}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+    </div>
+  );
+
   return (
     <div className="header text-light px-3 py-2 d-flex flex-column">
       <div
-        className={`header-top w-100 d-flex justify-content-${hideSearch ? 'center' : 'between'} align-items-center align-self-center position-relative`}
+        className={`header-top w-100 d-flex justify-content-${hideSearch ? 'center' : 'between'} align-items-center align-self-center`}
       >
         <a href="/profile">
           <button
@@ -87,6 +113,7 @@ const Header: React.FC<Props> = (props) => {
         {hideSearch ? null : renderSearchButton()}
       </div>
       {renderSearchBar()}
+      {categories?.length ? renderCategories() : null}
     </div>
   );
 };
